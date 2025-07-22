7346530a3e3337dd841fb8561110f7588d6ae13d
ZEShionYorigami <manuelninoadolfo18@gmail.com>
2025-07-22 18:54:12 +0800

    Initial Commit

---
+++ objectedition/patch.js
@@ -0,0 +1,10 @@
+{
+    "id": "object07",
+    "servers": [
+        "https://mirrors.thpatch.net/poorestgoddess18/object07/"
+    ],
+     "object07": "Replaces characters with object variants!",
+    "dependencies": [
+        "repos/nmlgc/base_tsa/"
+    ]
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "doublepeater",
+    "id": "poorestgoddess18",
+    "patches": {
+        "object07": "Replaces characters with object variants!",
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/poorestgoddess18/"
+    ],
+    "title": "poorestgoddess18 stuff"
+}
